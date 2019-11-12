c10061b70a09b4dea6ed9e24fe47288b5cd4fbfa
Kristina <30chris02@gmail.com>
2019-11-12 16:34:29 +0300

    Добавлен  простой todolist

---
+++ todo-simple/src/components/AddToDo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/App.css';
+
+const AddTodo = () => {
+
+	return (
+		<div className="toDo">
+			<form>
+				<input/>
+				<button type="submit">
+					Add
+				</button>
+			</form>
+		</div>
+	)
+};
+
+export default AddTodo
